00ac567d252797ed7558be49a544035161e0736e
Camila <piva.camila@hotmail.com>
2019-12-03 12:14:11 -0300

    Projeto

---
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -0,0 +1,69 @@
+<?php 
+
+namespace Hcode\DB;
+
+class Sql {
+
+	const HOSTNAME = "216.172.160.199";
+	const USERNAME = "abrace53_user";
+	const PASSWORD = "pdxSJ5}jjCtF";
+	const DBNAME = "abrace53_db";
+
+	private $conn;
+
+	public function __construct()
+	{
+
+		$this->conn = new \PDO(
+			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME, 
+			Sql::USERNAME,
+			Sql::PASSWORD
+		);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
